1b803738862698e584b7b33af99f75e6d20bc182
valentin` <lorishuon8@gmail.com>
2024-12-13 13:50:00 +0000

    modif temps chargment

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.onload = function () {
   setTimeout(function () {
       document.getElementById('loading-screen').style.display = 'none';
       document.getElementById('main-content').style.display = 'block';
-  }, 200);
+  }, 250);
 };
 
 triggerImage.addEventListener("click", (e) => {
