dbc2c5475a76de3115ef385930484dcd88c8cc35
valentin` <lorishuon8@gmail.com>
2024-12-13 17:36:23 +0000

    reparation flocon

--- script.js
+++ script.js
@@ -3,37 +3,6 @@ const warningMessage = document.getElementById("warning");
 const triggerImage = document.getElementById("trigger-image");
 const navbar = document.getElementById("navbar");
 
-window.onload = function () {
-  setTimeout(function () {
-      document.getElementById('loading-screen').style.display = 'none';
-      document.getElementById('main-content').style.display = 'block';
-  }, 250);
-};
-
-triggerImage.addEventListener("click", (e) => {
-  e.stopPropagation();
-  navbar.classList.toggle("visible");
-
-  if (navbar.classList.contains("visible")) {
-    triggerImage.style.display = "none"; 
-  } else {
-    triggerImage.style.display = "block";
-  }
-});
-
-// Fermer la barre si on clique en dehors
-document.addEventListener("click", (e) => {
-  if (!navbar.contains(e.target) && !triggerImage.contains(e.target)) {
-    navbar.classList.remove("visible");
-    triggerImage.style.display = "block";
-  }
-});
-
-// Fermer l'avertissement au clic sur le bouton
-closeWarningButton.addEventListener("click", () => {
-  warningMessage.classList.add("hidden");
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const maxSnowflakes = 50; // Nombre maximum de flocons actifs
   let currentSnowflakes = 0; // Compteur de flocons actifs
@@ -63,4 +32,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Créer des flocons à un rythme modéré
   setInterval(createSnowflake, 200); // Chaque 200ms, essayer d'ajouter un nouveau flocon
+});
+
+window.onload = function () {
+  setTimeout(function () {
+      document.getElementById('loading-screen').style.display = 'none';
+      document.getElementById('main-content').style.display = 'block';
+  }, 250);
+};
+
+triggerImage.addEventListener("click", (e) => {
+  e.stopPropagation();
+  navbar.classList.toggle("visible");
+
+  if (navbar.classList.contains("visible")) {
+    triggerImage.style.display = "none"; 
+  } else {
+    triggerImage.style.display = "block";
+  }
+});
+
+// Fermer la barre si on clique en dehors
+document.addEventListener("click", (e) => {
+  if (!navbar.contains(e.target) && !triggerImage.contains(e.target)) {
+    navbar.classList.remove("visible");
+    triggerImage.style.display = "block";
+  }
+});
+
+// Fermer l'avertissement au clic sur le bouton
+closeWarningButton.addEventListener("click", () => {
+  warningMessage.classList.add("hidden");
 });
